cfa83445c805787b54350a567ebd1bf308db6514
satishkumarvarma <satishkumarv752@gmail.com>
2025-07-07 20:28:15 +0530

    2nd part completed - ui design

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,3 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
